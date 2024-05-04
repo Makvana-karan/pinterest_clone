e8a847d2c74649cb1de1a814f15f29b8ce76c0ce
Makvana-karan <karanmakwana402@gmail.com>
2024-05-04 09:19:19 +0530

    save

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,13 +1,22 @@
-"use client"
-import React from 'react'
-import { SessionProvider } from "next-auth/react"
+"use client";
+// import React, { Children } from "react";
+// import { SessionProvider } from "next-auth/react";
 
-function Provider({children}) {
-  return (
-    <SessionProvider >
-        {children}
-    </SessionProvider>
-  )
+// function Provider({ children }) {
+//   return <SessionProvider>{children}</SessionProvider>;
+// }
+
+// export default Provider;
+
+import React, { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+
+interface ProviderProps {
+  children: ReactNode;
 }
 
-export default Provider
+function Provider({ children }: ProviderProps) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+
+export default Provider;
